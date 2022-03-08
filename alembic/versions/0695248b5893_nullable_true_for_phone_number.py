8225c24a4bc760541cb6ed52c0d8e38695a6736b
justine12 <justorang@gmail.com>
2022-03-08 16:04:09 +0300

    nullable phone number

---
+++ alembic/versions/0695248b5893_nullable_true_for_phone_number.py
@@ -0,0 +1,26 @@
+"""nullable=true for phone number
+
+Revision ID: 0695248b5893
+Revises: ac299b385f8a
+Create Date: 2022-03-08 15:44:56.726951
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0695248b5893'
+down_revision = 'ac299b385f8a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+
+    pass
+
+
+def downgrade():
+
+    pass
